4a35d7e8f71e6e41ea83cb2165bc3d93181d1166
Hòa Nguyễn <phuchoa099@gmail.com>
2020-04-23 10:34:13 +0700

    Handler the Object Viewed Signal and Ending User Session

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -5,6 +5,7 @@ from django.utils.http import is_safe_url
 
 from .forms import LoginForm, RegisterForm, GuestForm
 from .models import GuestEmail
+from .signals import user_logged_in
 
 # Create your views here.
 
@@ -43,6 +44,7 @@ class LoginView(FormView):
         print(user)
         if user is not None:
             login(request, user)
+            user_logged_in.send(user.__class__, instance=user, request=request)
             try:
                 del request.session['guest_email_id']
             except:
--- src/analytics/models.py
+++ src/analytics/models.py
@@ -8,8 +8,12 @@ from django.db.models.signals import post_save, pre_save
 
 from .signals import object_viewed_signal
 from .utils import get_client_ip
+from accounts.signals import user_logged_in
 
 User = settings.AUTH_USER_MODEL
+
+FORCE_SESSION_TO_ONE = getattr(settings, 'FORCE_SESSION_TO_ONE', False)
+FORCE_INACTIVE_USER_ENDSESSION = getattr(settings, 'FORCE_INACTIVE_USER_ENDSESSION', False)
 
 class ObjectViewed(models.Model):
     user = models.ForeignKey(User, null=True, blank=True, on_delete=models.DO_NOTHING)
@@ -30,7 +34,7 @@ class ObjectViewed(models.Model):
         verbose_name = 'Object viewed'
         verbose_name_plural = 'Objects viewed'
 
-def object_viewed_reciever(sender, instance, request, *args, **kwargs):
+def object_viewed_receiver(sender, instance, request, *args, **kwargs):
     c_type = ContentType.objects.get_for_model(sender) #instance.__class__
     new_view_object = ObjectViewed.objects.create(
         user=request.user,
@@ -40,4 +44,61 @@ def object_viewed_reciever(sender, instance, request, *args, **kwargs):
     )
     
 
-object_viewed_signal.connect(object_viewed_reciever)
+object_viewed_signal.connect(object_viewed_receiver)
+
+# UserSession Class
+class UserSession(models.Model):
+    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.DO_NOTHING)
+    ip_address = models.CharField(max_length=220, null=True, blank=True)
+    session_key = models.CharField(max_length=120, null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+    ended = models.BooleanField(default=False)
+
+    def end_session(self):
+        session_key = self.session_key
+        ended = self.ended
+        try:
+            Session.objects.get(pk=session_key).delete()
+            self.active = False
+            self.ended = True
+            self.save()
+        except:
+            pass
+        return self.ended
+
+def post_save_session_receiver(sender, instance, created, *args, **kwargs):
+    if created:
+        qs = UserSession.objects.filter(user=instance.user, ended=False, active=True).exclude(id=instance.id)
+        for i in qs:
+            i.end_session()
+    if not instance.active and not instance.ended:
+        instance.end_session() 
+if FORCE_SESSION_TO_ONE:
+    post_save.connect(post_save_session_receiver, sender=UserSession)
+
+
+def post_save_user_change_receiver(sender, instance, created, *args, **kwargs):
+    if not created:
+        if instance.is_active == False:
+            qs = UserSession.objects.filter(user=instance.user, active=False, ended=False)
+            for i in qs:
+                i.end_session()
+if FORCE_INACTIVE_USER_ENDSESSION:
+    post_save.connect(post_save_user_change_receiver, sender=User)
+
+
+
+def user_logged_in_receiver(sender, instance, request, *args, **kwargs):
+    user = instance
+    session_key = request.session.session_key
+    ip_address = get_client_ip(request)
+    UserSession.objects.create(
+        user=user,
+        ip_address=ip_address,
+        session_key=session_key
+    )
+
+user_logged_in.connect(user_logged_in_receiver)
+
+
