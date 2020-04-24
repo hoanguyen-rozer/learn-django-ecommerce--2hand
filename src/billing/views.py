923c22e0ab0094a07edde8e59db77e7524ba6bf5
Hòa Nguyễn <phuchoa099@gmail.com>
2020-04-25 02:13:16 +0700

    Add card to Customer with Stripe

--- src/billing/views.py
+++ src/billing/views.py
@@ -1,15 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.utils.http import is_safe_url
 # Create your views here.
 
 import stripe
 from django.http import HttpResponse, JsonResponse
+from .models import BillingProfile
 
 stripe.api_key = 'sk_test_u7cis2zbqN2oQmy7SflQuu1X00iCYdUUXf'
 
 STRIPE_PUB_KEY = 'pk_test_W1bM1qmnrcOQ780IojSHaUcY00AOjvrMTI'
 
 def payment_method_view(request):
+
+    # if request.user.is_authenticated():
+    #     billing_profile = request.user.billingprofile
+    #     customer_id = billing_profile.customer_id
+
+    billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
+    if not billing_profile:
+        return redirect('/cart/')
+
     next_url = None
     next_ = request.GET.get('next')
     if is_safe_url(next_, request.get_host()):
@@ -22,6 +32,11 @@ def payment_method_view(request):
 
 def payment_method_createview(request):
     if request.method == "POST" and request.is_ajax():
-        print(request.POST)
+        billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
+        if not billing_profile:
+            return HttpResponse({'message': 'Cannot find this user'}, status_code=401)
+        token = request.POST.get('token')
+        if token is not None:
+            stripe.Customer.create_source(billing_profile.customer_id, source=token, )
         return JsonResponse({'message': 'Done'})
-    return HttpResponse('error', status_code=401)
+    return HttpResponse({'message': 'error'}, status_code=401)
---
+++ src/staticfiles/js/ecommerce.main.js
@@ -0,0 +1,135 @@
+$(document).ready(function(){
+
+var stripeFormModule = $(".stripe-payment-form")
+  var stripeModuleToken = stripeFormModule.attr("data-token")
+  var stripeModulNextUrl = stripeFormModule.attr("data-next-url")
+  var stripeModuleBtnTitle = stripeFormModule.attr("data-btn-title") || "Add card"
+  var stripeTemplate = $.templates("#stripeTemplate")
+  var stripeTemplateDataContext = {
+    publishKey: stripeModuleToken,
+    nextUrl: stripeModulNextUrl,
+    btnTitle: stripeModuleBtnTitle
+  }
+  var stripeTemplateHtml = stripeTemplate.render(stripeTemplateDataContext)
+  stripeFormModule.html(stripeTemplateHtml)
+
+// http secure site when live
+
+var paymentForm = $('.payment-form')
+if (paymentForm.length > 1) {
+    alert("Only one payment form is allowed per page")
+    paymentForm.css('display', 'none')
+}
+else if (paymentForm.length == 1){
+
+var pubKey = paymentForm.attr('data-token')
+var nextUrl = paymentForm.attr('data-next-url')
+
+    // Create a Stripe client.
+var stripe = Stripe(pubKey);
+
+// Create an instance of Elements.
+var elements = stripe.elements();
+
+// Custom styling can be passed to options when creating an Element.
+// (Note that this demo uses a wider set of styles than the guide below.)
+var style = {
+  base: {
+    color: '#32325d',
+    fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+    fontSmoothing: 'antialiased',
+    fontSize: '16px',
+    '::placeholder': {
+      color: '#aab7c4'
+    }
+  },
+  invalid: {
+    color: '#fa755a',
+    iconColor: '#fa755a'
+  }
+};
+
+// Create an instance of the card Element.
+var card = elements.create('card', {style: style});
+
+// Add an instance of the card Element into the `card-element` <div>.
+card.mount('#card-element');
+
+// Handle real-time validation errors from the card Element.
+card.addEventListener('change', function(event) {
+  var displayError = document.getElementById('card-errors');
+  if (event.error) {
+    displayError.textContent = event.error.message;
+  } else {
+    displayError.textContent = '';
+  }
+});
+
+// Handle form submission.
+var form = document.getElementById('payment-form');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  stripe.createToken(card).then(function(result) {
+    if (result.error) {
+      // Inform the user if there was an error.
+      var errorElement = document.getElementById('card-errors');
+      errorElement.textContent = result.error.message;
+    } else {
+      // Send the token to your server.
+      stripeTokenHandler(nextUrl, result.token);
+    }
+  });
+});
+
+// Submit the form with the token ID.
+function stripeTokenHandler(nextUrl, token) {
+    console.log(token)
+    var paymentMethodEndpoint = '/billing/payment-method/create/'
+    var data = {
+        'token': token.id
+    }
+    function redirectToNext(nextPath){
+        if (nextPath){
+            setTimeout(function(){
+                window.location.href = nextPath
+            }, 150)
+        } 
+    }
+
+    $.ajax({
+        data: data,
+        url: paymentMethodEndpoint,
+        method: "POST",
+        success: function(data){
+            var successMsg = "Success! your card was added"
+            card.clear()
+            // if (nextUrl){
+            //     window.location.href = nextUrl
+            //}else {
+            //     window.location.reload()
+            // }
+            alert(successMsg)
+            redirectToNext(nextUrl)
+        },
+        erorr: function(erorr){
+            console.log(error)
+        }
+    })
+
+
+
+  // Insert the token ID into the form so it gets submitted to the server
+//   var form = document.getElementById('payment-form');
+//   var hiddenInput = document.createElement('input');
+//   hiddenInput.setAttribute('type', 'hidden');
+//   hiddenInput.setAttribute('name', 'stripeToken');
+//   hiddenInput.setAttribute('value', token.id);
+//   form.appendChild(hiddenInput);
+
+//   // Submit the form
+//   form.submit();
+}
+}
+    
+})
